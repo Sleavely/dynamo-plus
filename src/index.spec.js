5725a01ddc2865e2949bbcf5f190c523ce227d7e
Joakim Hedlund <joakim.hedlund@danielwellington.com>
2019-05-28 20:37:10 +0200

    Use inline folder structure for tests. Dont include tests in npm package.

---
+++ src/index.spec.js
@@ -0,0 +1,26 @@
+
+jest.doMock('./promisifyDocumentClient')
+jest.doMock('./retryableExceptions')
+
+const requireUncached = (moduleName) => {
+  delete require.cache[require.resolve(moduleName)]
+  return require(moduleName)
+}
+
+describe('dynamo-plus', () => {
+  it('can be loaded without throwing exceptions', () => {
+    expect(() => {
+      requireUncached(`./index`)
+    }).not.toThrow()
+  })
+  it('has DynamoPlus property', () => {
+    const dp = requireUncached(`./index`)
+    expect(dp).toHaveProperty('DynamoPlus')
+  })
+  it('DynamoPlus() returns a new DocumentClient', () => {
+    const { DynamoPlus } = requireUncached(`./index`)
+    const client = DynamoPlus()
+
+    expect(client.constructor.name).toBe('DocumentClient')
+  })
+})
