8b0f9921ee44ccc0ed1210eccce5321f990c4af0
Joakim Hedlund <joakim.hedlund@danielwellington.com>
2019-06-04 19:30:40 +0200

    Clarify purpose

--- src/clientMethods.spec.js
+++ src/clientMethods.spec.js
@@ -6,6 +6,7 @@ it('exposes a list of methods to mutate', () => {
   expect(methods).toBeInstanceOf(Array)
 })
 
+// Dont want to modify something that isn't actually a real method
 it.each(methods)('%s() exists on DocumentClient', (method) => {
   expect(DocumentClient.prototype).toHaveProperty(method)
 })
