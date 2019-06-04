8939bbc8cd29328434dc0c8fb2a30a530255fdb3
Joakim Hedlund <joakim.hedlund@danielwellington.com>
2019-06-04 19:27:15 +0200

    Clarify whats going on

--- src/retryableExceptions.js
+++ src/retryableExceptions.js
@@ -13,13 +13,23 @@ const retryableExceptions = [
 
 const autoRetry = (client) => {
   clientMethods.forEach((method) => {
+    // Save a reference to the unmutated version of the method.
+    // We're not storing it on the client as a prop since
+    // it is probably not the original original.
+    const originalMethod = client[method]
+
     client[method] = async (params = {}) => {
-      return client[method](params).catch((err) => {
-        if (retryableExceptions.some((retry) => [err.constructor.name, err.name].includes(retry))) {
+      return originalMethod(params).catch((err) => {
+        const retryableException = retryableExceptions.find((retry) => [err.constructor.name, err.name].includes(retry))
+
+        if (retryableException) {
+          // Back off by 150% every time,
+          // starting at 1 sec and a maximum of 30 sec between tries.
           params.lastBackOff = params.lastBackOff ? Math.min(Math.floor(params.lastBackOff * 1.5), 30000) : 1000
           return sleep(params.lastBackOff)
             .then(() => client[method](params))
         }
+
         // Non-retryable exceptions (like ValidationException) should incur actual error propagation
         throw err
       })
