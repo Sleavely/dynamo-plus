ae903b0c368b15b47de69d1089ee2dc97c9c9b16
Joakim Hedlund <joakim.hedlund@danielwellington.com>
2019-06-19 17:00:05 +0200

    Avoid promisifying non-existent methods

--- src/promisifyDocumentClient.js
+++ src/promisifyDocumentClient.js
@@ -8,6 +8,12 @@ const methodsToPromisify = require('./clientMethods')
  */
 const promisifyDocumentClient = (client) => {
   methodsToPromisify.forEach((method) => {
+    // Depending on aws-sdk version, transactions might not be available
+    if (!client[method]) {
+      delete methodsToPromisify[method]
+      return undefined
+    }
+
     client[`original_${method}`] = client[method]
     client[method] = async (params = {}) => {
       return client[`original_${method}`](params).promise()
