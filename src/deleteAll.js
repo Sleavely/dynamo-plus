222b97564218589bb7edfd46a5a4512aaf510b0d
Joakim Hedlund <joakim.hedlund@danielwellington.com>
2019-11-07 15:18:48 +0100

    .deleteAll() and tests

---
+++ src/deleteAll.js
@@ -0,0 +1,26 @@
+
+const batchWriteRetry = require('./utils/batchWriteRetry')
+const chunk = require('./utils/chunk')
+
+exports.appendDeleteAll = (client) => {
+  client.deleteAll = async (params = {}) => {
+    const {
+      TableName = '',
+      Keys = [],
+      BatchSize = 25,
+    } = params
+
+    const batches = chunk(Keys, BatchSize)
+
+    // Build a chain of batchWrite operations
+    return batches.reduce(async (chain, batch) => {
+      await chain
+
+      return batchWriteRetry(client, {
+        RequestItems: {
+          [TableName]: batch.map((key) => ({ DeleteRequest: { Key: key } }))
+        }
+      })
+    }, Promise.resolve())
+  }
+}
