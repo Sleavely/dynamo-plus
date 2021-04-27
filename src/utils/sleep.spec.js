fbb2e38f559dcb6465ee48647367114028972ebe
Joakim Hedlund <contact@joakimhedlund.com>
2021-04-27 09:44:06 +0200

    Use Jest's fake timers - fixes #24

--- src/utils/sleep.spec.js
+++ src/utils/sleep.spec.js
@@ -1,23 +1,35 @@
 
 const sleep = require('./sleep')
 
+jest.useFakeTimers()
+
+// Avoid leakage
+beforeEach(() => {
+  jest.runAllTimers()
+})
+afterEach(() => {
+  jest.runAllTimers()
+})
+
 it('returns a promise', async () => {
   expect(sleep()).toBeInstanceOf(Promise)
 })
 
 it('resolves after a custom amount of time', async () => {
-  // Time travel in tests is always hell. Jest provides facilities
-  // for this but I couldn't for the life of me get it working, so..
-  const startTime = Date.now()
   const expectedSleepTime = 50
-  const margin = 20
-  await expect(sleep(expectedSleepTime)).toResolve()
-  const finishedAt = Date.now() - startTime
-  expect(finishedAt).toBeLessThan(expectedSleepTime + margin)
-  expect(finishedAt).toBeGreaterThan(margin)
+
+  const sleeperPromise = sleep(expectedSleepTime)
+  jest.runAllTimers()
+
+  await expect(sleeperPromise).toResolve()
+  expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), expectedSleepTime)
 })
 
 it('can resolve with a custom value', async () => {
   const customValue = 'Rocinante'
-  await expect(sleep(50, customValue)).resolves.toBe(customValue)
+
+  const sleeperPromise = sleep(50, customValue)
+  jest.runAllTimers()
+
+  await expect(sleeperPromise).resolves.toBe(customValue)
 })
