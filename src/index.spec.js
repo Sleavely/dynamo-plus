29a12667078b5d1e01b8ed685e5a444f47191c6c
Joakim Hedlund <contact@joakimhedlund.com>
2021-09-12 21:01:03 +0200

    Dont inject keep-alive when endpoint has been overridden

--- src/index.spec.js
+++ src/index.spec.js
@@ -26,6 +26,7 @@ describe('DynamoPlus', () => {
 
     expect(client.constructor.name).toBe('DocumentClient')
   })
+
   it('allows custom options', () => {
     const { DynamoPlus } = requireUncached(`./index`)
 
@@ -33,11 +34,40 @@ describe('DynamoPlus', () => {
 
     expect(client.service.config.potato).toBe('Hello')
   })
-  it('defaults to a keepalive-enabled http agent', () => {
+
+  it('defaults to using a keepalive-enabled https agent', () => {
     const { DynamoPlus } = requireUncached(`./index`)
 
     const client = DynamoPlus()
 
-    expect(client.service.config.httpOptions).toMatchObject({ agent: { keepAlive: true } })
+    const clientConfig = client.service.config
+    const apiRequest = client.get({ TableName: 'test', Key: { id: 'test' } })
+
+    expect(clientConfig.httpOptions).toMatchObject({ agent: { keepAlive: true } })
+    expect(apiRequest).toMatchObject({ service: { config: { httpOptions: { agent: { keepAlive: true } } } } })
+  })
+
+  it('does not set an agent if a custom endpoint is defined', async () => {
+    const { DynamoPlus } = requireUncached(`./index`)
+
+    const client = DynamoPlus({ region: 'eu-west-1', endpoint: 'http://localhost:7171/' })
+
+    const clientConfig = client.service.config
+    const apiRequest = client.get({ TableName: 'test', Key: { id: 'test' } })
+
+    expect(clientConfig.httpOptions).toMatchObject({ agent: undefined })
+    expect(apiRequest).toMatchObject({ service: { config: { httpOptions: { agent: undefined } } } })
+  })
+
+  it('allows an explicitly undefined http(s) agent', async () => {
+    const { DynamoPlus } = requireUncached(`./index`)
+
+    const client = DynamoPlus({ region: 'eu-west-1', httpOptions: { agent: undefined } })
+
+    const clientConfig = client.service.config
+    const apiRequest = client.get({ TableName: 'test', Key: { id: 'test' } })
+
+    expect(clientConfig.httpOptions).toMatchObject({ agent: undefined })
+    expect(apiRequest).toMatchObject({ service: { config: { httpOptions: { agent: undefined } } } })
   })
 })
